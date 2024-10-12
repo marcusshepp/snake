3ef451f3b0e9fbc279c002361a2ee688d3bf6512
Marcus Shepherd <marcusshepdotcom@gmail.com>
2024-10-12 06:27:11 -0400

    scarface

--- v2/src/main.ts
+++ v2/src/main.ts
@@ -1,18 +1,15 @@
-import { SnakeHead } from './snake';
+import { SnakeHead } from "./snake";
 
-const canvas = document.getElementById('can') as HTMLCanvasElement;
-const content = canvas.getContext('2d') as CanvasRenderingContext2D;
+const canvas = document.getElementById("can") as HTMLCanvasElement;
+const content = canvas.getContext("2d") as CanvasRenderingContext2D;
 const BLOCK_WIDTH: number = 50;
 const BLOCK_HEIGHT: number = 50;
 
 let snakeHead: SnakeHead;
 
 function drawBoard() {
-    // const colors = ['black', 'yellow', 'blue', 'green', 'red'];
-    // colors[Math.floor(Math.random() * colors.length)]
     for (let x = 0; x < 800; x += 50) {
         for (let y = 0; y < 600; y += 50) {
-            // content.fillStyle = 'black';
             content.beginPath();
             content.rect(x, y, BLOCK_HEIGHT, BLOCK_WIDTH);
             content.stroke();
@@ -21,7 +18,7 @@ function drawBoard() {
 }
 
 function drawBlock() {
-    content.fillStyle = 'blue';
+    content.fillStyle = "blue";
     content.fillRect(snakeHead.x, snakeHead.y, BLOCK_HEIGHT, BLOCK_WIDTH);
 }
 
